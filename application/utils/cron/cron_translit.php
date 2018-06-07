d06ca4c880b8136b000c972f459af384ce4c6367
Ryabikovalex <ryabikov.alex@gmail.com>
2018-06-07 21:47:45 +0300

    Updated view

--- application/utils/cron/cron_translit.php
+++ application/utils/cron/cron_translit.php
@@ -1,13 +1,16 @@
 <?php
 ini_set('display_errors',1);
+$conf = parse_ini_file( '../../../config.ini', 1);
+include '../../core/Database.php';
+include '../translit_func.php';
+Database::instance('127.0.0.1:3366', 'kurocajy_shop', 'utf8', $conf['PDO']['user'], $conf['PDO']['password']);
 $logger = fopen('cron.log', 'a+');
-$log = [date('[h:i:s t-m-Y]')];
-
+$log = [];
 ///Обновление транслита в БД
 $t = ['country', 'city', 'region', 'categories'];
 foreach ($t as $v)
 {
-    /*$sql0 = 'SELECT `id`, `name` FROM `{r}`';
+    $sql0 = 'SELECT `id`, `name` FROM `{r}`';
     $sql01 = 'UPDATE `{r}` SET `translit`=? WHERE `id`=?';
     $sql = str_ireplace('{r}', $v, $sql0);
     $sql1 = str_ireplace('{r}', $v, $sql01);
@@ -16,15 +19,15 @@ foreach ($t as $v)
 
     for($i=0; $i < count($data); $i++)
     {
-        $translit = to_translit($data[$i]['name']);
+        $translit = str_ireplace(  [ '-obl.', '-kray', '-biznes'], '', to_translit($data[$i]['name']) );
         Database::run($sql1, [ $translit, $data[$i]['id']]);
-    }*/
+    }
     $log[] = 'Транслитерация произедена успешно в таблице '.strtoupper($v).'';
 }
 
 
 foreach ($log as $m)
 {
-    fwrite($logger,$m.PHP_EOL);
+    fwrite($logger,date('[h:i:s t-m-Y]').' : '.$m.PHP_EOL);
 }
 fclose($logger);
