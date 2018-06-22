01bd4a98e6186beaa65ce9bde393cb1b62668b71
Ryabikovalex <ryabikov.alex@gmail.com>
2018-06-22 20:48:03 +0300

    Fix

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -91,12 +91,6 @@
                         Reports <span class="badge badge-secondary"><?php echo $stat['reports_count'] ?? 0;?></span>
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-layers"><polygon points="12 2 2 7 12 12 22 7 12 2"></polygon><polyline points="2 17 12 22 22 17"></polyline><polyline points="2 12 12 17 22 12"></polyline></svg>
-                        Integrations
-                    </a>
-                </li>
             </ul>
 
             <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
@@ -105,14 +99,12 @@
             <ul class="nav flex-column mb-2">
                 <li class="nav-item">
                     <a class="nav-link" href="/show/submit_buyers">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
-                        Новые заявки на покупку <span class="badge badge-secondary"><?php echo $stat['submit_buyers_count'] ?? 0;?></span>
+                        Заявки на покупку <span class="badge badge-secondary"><?php echo $stat['submit_buyers_count'] ?? 0;?></span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="/show/submit_products">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
-                        Новые бизнесы на продажу <span class="badge badge-secondary"><?php echo $stat['submit_products_count'] ?? 0;?></span>
+                        Бизнесы на продажу <span class="badge badge-secondary"><?php echo $stat['submit_products_count'] ?? 0;?></span>
                     </a>
                 </li>
             </ul>
